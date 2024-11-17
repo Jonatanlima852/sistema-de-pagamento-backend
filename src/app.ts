7d388ce8fc93554e75b76ab6745f92ba98def68d
Jonatan de Lima <jonatanlima852@gmail.com>
2024-11-17 00:15:02 -0300

    organizei middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,68 +1,23 @@
-import express, { ErrorRequestHandler, NextFunction, Request, Response } from 'express';
+import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJsdoc from 'swagger-jsdoc';
 import userRoutes from './routes/user.routes';
-import { AppError } from './utils/AppError';
-import { userSwaggerDocs } from './docs/user.swagger';
+import { swaggerOptions } from './config/swagger.config';
+import { errorHandler } from './middlewares/error.middleware';
+
 
 const app = express();
-
-// Middlewares de segurança
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-
-// Swagger setup
-const swaggerOptions = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API Documentation',
-      version: '1.0.0',
-      description: 'Documentação da API de Usuários',
-    },
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
-        },
-      },
-    },
-    paths: {
-      ...userSwaggerDocs
-    }
-  },
-  apis: []
-};
-
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// Rotas
-app.use('/api', userRoutes);
 
-// Middleware de tratamento de erros
-const errorHandler: ErrorRequestHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof AppError) {
-    res.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
-    });
-    return ;
-  }
+app.use(helmet()); // Middleware de segurança - protege a API contra algumas vulnerabilidades comuns
+app.use(cors()); // Middleware de segurança - habilita acesso a API de outras origens
+app.use(express.json()); // Middleware de parseamento de JSON
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs)); // Middleware de documentação
+app.use('/api', userRoutes); //Middleware de rotas
+app.use(errorHandler);   // Middleware de tratamento de erros
 
-  console.error(err);
-
-  res.status(500).json({
-    status: 'error',
-    message: 'Internal server error',
-  });
-};
-
-app.use(errorHandler);
 
 export default app;
---
+++ src/config/swagger.config.ts
@@ -0,0 +1,25 @@
+import { userSwaggerDocs } from '../docs/user.swagger';
+
+export const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API Documentation',
+      version: '1.0.0',
+      description: 'Documentação da API de Usuários',
+    },
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    paths: {
+      ...userSwaggerDocs
+    }
+  },
+  apis: []
+}; 
